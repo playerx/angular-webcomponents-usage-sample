f657340973aee90d2ac41872cab4cf56c8ad2f8b
Ezeki ZIbzibadze <ez@Green.local>
2017-12-10 02:07:03 +0400

    added material

---
+++ src/app/common/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+} from '@angular/material';
+
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+  ],
+})
+export class MaterialModule { }
